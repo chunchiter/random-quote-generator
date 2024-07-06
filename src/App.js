115a2b4b59ef10faff64a6dbba1074c5a6e6fe8f
Rafael A. Castañon Roman <rafacastajr5@gmail.com>
2024-07-06 17:13:57 -0600

    axios and quotes api default

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-const App = () =>{
-  const[quotes, setQuotes] = useState('');
+const App = () => {
+  const [quotes, setQuotes] = useState('');
 
   const getQuotes = () => {
-    fetch("https://waifu-it.p.rapidapi.com/waifu?name=Asuna%20Yuuki&anime=Sword%20Art%20Online")
-    .then((res) => res.json())
-    .then((data) => {
-      let randomNum = Math.floor(Math.random() * data.length);
-      setQuotes(data[randomNum]);
-    });
+    fetch("https://type.fit/api/quotes")
+      .then(response => response.json())
+      .then(data => {
+
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const randomQuote = data[randomIndex].text;
+        setQuotes(randomQuote);
+      })
   };
-  
+
   useEffect(() => {
     getQuotes();
-  }, []);
+  }, []); 
 
-  return(
-    <div className="App"> 
-        <div className="quotes">
-        <p>{quotes.text}</p>
-        <p>{quotes.author}</p>
-          <div className='btnContainer'>
-            <button onClick={getQuotes}>Get Quotes</button>
-            <a href='' className='btn'>Tweet</a>
-            <a href='' className='btn'>Instagram</a>
-            <a href='' className='btn'>Threads</a>
-          </div>
+  return (
+    <div className="App">
+      <div className="quotes">
+        <div className="btnContainer">
+          <button onClick={getQuotes}>Get Quotes</button>
+          <a href="" className="btn">Tweet</a>
+          <a href="" className="btn">Instagram</a>
+          <a href="" className="btn">Threads</a>
         </div>
+        {quotes && (
+          <div className="quote">
+            <p>{quotes}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
